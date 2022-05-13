9adcf25ccc96ca23daa741b32725471edf460379
felipbttk <felipbttk@hotmail.com>
2022-05-13 14:24:24 +0200

    commit on 'main'

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <div className='app__header app__wrapperr section__paddingg' >
     <div className='app__wrapper_infoo'>
         <SubHeading title='Welcome on Board'/>
-        <h1 className='app__header-h1'>The Key To Yachting Formalities</h1>
+        <h1 className='app__header-h1'>The Key To Yachtinggg Formalities</h1>
         {/* <p className='p__opensans' style={{ margin: '2rem 0' }}>This is a comment for what we serve about our products  and our coltoure</p> */}
       <a href="#services">  <button type='button' className='custom__buttonn'>Our Servises</button> </a>
     </div>
